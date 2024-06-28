7694d3e20d9fb0bd83428baa6cdacafbf77f0ca4
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-06-29 03:57:35 +0500

    Add From Files

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+addition = num1 + num2
+print("Addition: "+str(addition))
+
+substraction = num1-num2
+print("Substraction: "+str(substraction))
+
+multiplication = num1*num2
+print("Multiplication:"+str(multiplication))
+
+division = num1/num2
+print("Division:"+str(division))
+
+floor_division = num1//num2
+print("Floor_division: "+str(floor_division))
+
+exponential = num1**num2
+print("Exponentail:" +str(exponential))
+
+modulus = num1%num2
+print("Modulus: "+str(modulus))
