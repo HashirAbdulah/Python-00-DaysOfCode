e4fea88797630e2a3f0ec295489251a8b146a1cf
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-06-28 17:16:36 +0500

    Added from files

---
+++ Pip&Modules.py
@@ -0,0 +1,9 @@
+# This Is the pip and modules concept
+# Example:
+
+import pandas
+import os
+    # os module in Python provides functions for interacting with the operating system.
+
+# This link shows the modules provided by Pyhton
+# https://docs.python.org/3/py-modindex.html
