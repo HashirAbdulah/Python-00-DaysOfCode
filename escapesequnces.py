04d42d949bc16b309a23a3b9c2306bdb28cc5591
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-06-29 03:36:55 +0500

    Added from Files

---
+++ escapesequnces.py
@@ -0,0 +1,13 @@
+#Newline character (\n) in Python
+name = input("Enter your name:")
+print("This is " + name)
+print(name + " is a good boy  \n\"having python skills\"")
+
+
+# Multiline strings in Python
+
+multiLineString = """This is a multi-line string.
+It can span across multiple lines.
+This is the end."""
+print(multiLineString)
+
---
+++ variables.py
@@ -0,0 +1,17 @@
+a = input("Enter ur name: ")
+print(type(a))
+
+list = [1,3,5,7,8]
+print(list)
+print(type(list))
+#List  are mutable
+
+tuple = (1,5,9,3,7)
+print(tuple)
+print(type(tuple))
+#Tuple are immutable
+
+fatherName = input("Enter Father name: ")
+age = input("Enter your age: ")
+dict = {"name": a, "age": age, "FatherName":fatherName, "CanVote": True}
+print(dict)
