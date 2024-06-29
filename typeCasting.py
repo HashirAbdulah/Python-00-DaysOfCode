66d65b6039bacd7478f130bc8b46b5129f7a294b
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-06-29 12:21:20 +0500

    Added

---
+++ typeCasting.py
@@ -0,0 +1,28 @@
+x = 5
+y = float(x)
+print(y)  # Output: 5.0
+
+
+x = 3.7
+y = int(x)
+print(y)  # Output: 3
+
+x = "123"
+y = int(x)
+print(y)  # Output: 123
+
+x = 456
+y = str(x)
+print(y)  # Output: "456"
+
+x = "3.14"
+y = float(x)
+print(y)  # Output: 3.14
+
+x = [1, 2, 3]
+y = tuple(x)
+print(y)  # Output: (1, 2, 3)
+
+x = (4, 5, 6)
+y = list(x)
+print(y)  # Output: [4, 5, 6]
