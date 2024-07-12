cf645f1811360fc50b30f3f09e3b62d573d49329
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-07-13 03:12:27 +0500

    Added

---
+++ BreakAndContinue.py
@@ -0,0 +1,12 @@
+for i in range(1,9):
+    if i == 8:
+        break
+    print(i)
+print("As the condition is that i == 8 break the loop so the output is 1-7 suspending 8 while range is even from 1-9")
+
+for i in range(1,9):
+    if i == 1:
+        continue
+    print(i)
+print("As the condition is that i == 1 continye the loop so the output is 2-8 suspending 1 while range is even from 1-9")
+
