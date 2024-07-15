d8a78d092e382daffb41a727809d5aeacba6b7e1
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-07-16 00:09:58 +0500

    Added

---
+++ ListMethods.py
@@ -0,0 +1,17 @@
+list = [1,2,3,4,5,6]
+print(list)
+# Append method: Add a elements at the last index of a List 
+list.append(7)
+print("Appended list:",list)
+
+# Sort method: Sorts the element of the List in ascending order
+list1 = [1,7,3,4,6,0,2]
+print(list1)
+list1.sort()
+print("Ascending Sorted List:",list1)
+
+# Sort(reverse = True) method: Sorts the element of the List in Descending order
+list2 = [1,7,3,4,6,0,2]
+print(list2)
+list2.sort(reverse=True)
+print("Descending Sorted List",list2)
