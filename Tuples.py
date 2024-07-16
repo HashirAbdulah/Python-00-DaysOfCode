aef2c4cc381c07780f9c7903de8e4a6a21e0fd14
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-07-16 18:59:54 +0500

    Added

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+tup = (1,2,4,6,"English",True,)
+print(type(tup))
+for i in range(0,len(tup)):
+    print(tup[i])
+    
+    # Tuples are same as Lists(Arrays) but are immutable and can not be changed
+    # Tuples can only be changed ,adding,removing only after converting into list and then again for secure lists change into tupel 
