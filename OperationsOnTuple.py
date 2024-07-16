c7abf1a2d48c55fb1095f49538cc5325d6ad7bb6
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-07-16 19:59:16 +0500

    Add

--- OperationsOnTuple.py
+++ OperationsOnTuple.py
@@ -9,3 +9,27 @@ tupple = tuple(temp_list)
 print(tupple)
 
 # First change the tuple to list then add element at the end then change the list again to tuple
+
+# Concatination of tuples:
+tuple1 = ("Pakistan","Palestine","Canada")
+tuple2 = ("America","UAE","England")
+tuple3 = ("Iran","Sharjah","Bangladesh")
+concatinatedTuple = tuple1 + tuple2 + tuple3
+print(concatinatedTuple)
+
+
+# Count Method : Counts the Occurance of the element
+tpl = (1,1,1,4,5,6,8,9,3,3,3,5)
+res = tpl.count(3)
+print(res)
+
+# Index Method: Give the element first occurance of the Particular element in ()
+
+res2 = tpl.index(5)
+print(res2)
+
+# Len Method : Gives the Length of the Tuple
+res3 = len(tpl)
+print(res3)
+
+# You can use every list method on tuple by converting the tuple to the list
