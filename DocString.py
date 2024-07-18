747a47e4427d629bf62974fddb7f27ce26c27bee
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-07-19 03:14:03 +0500

    Added

---
+++ DocString.py
@@ -0,0 +1,9 @@
+def square (n):
+    """This function returns the square of a numner"""
+    print(square.__doc__)
+    print(n**2)
+
+square(int(input("Enter number for Square: ")))
+
+# The lne in the triple quotes are the docString used as comments but also can be printed it also right above or right below adter the function name
+
