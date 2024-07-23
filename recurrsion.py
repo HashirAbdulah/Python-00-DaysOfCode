d95303d3817acc4a7bcf703d67cb271bbdadcb6b
Hashir Abdullah <hashirabdullah46@gmail.com>
2024-07-23 22:37:57 +0500

    Added

---
+++ recurrsion.py
@@ -0,0 +1,23 @@
+n = int(input("Enter number for factorial :"))
+def factorial (n):
+    if (n == 0 or n == 1):
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print("Factorial of",n, "is",factorial(n))
+
+
+
+number  = int(input("Enter a Number for fibonacci sequence:"))
+def fibonacciSequence (number):
+        if number < 0 :
+            print("Incorrect input,Input Number Greater than 0")
+        elif (number == 0):
+            return 0
+        elif(number == 1 or number == 2):
+                return 1
+        else:
+            return fibonacciSequence(number-1) + fibonacciSequence(number-2)
+        
+print("Fibonacci sequence upto",number, "is", fibonacciSequence(number))
