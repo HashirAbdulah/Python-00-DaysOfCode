509959e1df46da032cdae2f9deb8fe239d837362
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-20 17:21:47 +0500

    Added

--- escapesequnces.py
+++ escapesequnces.py
@@ -1,13 +1,21 @@
 #Newline character (\n) in Python
 name = input("Enter your name:")
-print("This is " + name)
+print("This is ", name)
 print(name + " is a good boy  \n\"having python skills\"")
 
 
-# Multiline strings in Python
+# # Multiline strings in Python
 
-multiLineString = """This is a multi-line string.
-It can span across multiple lines.
-This is the end."""
-print(multiLineString)
+# multiLineString = """This is a multi-line string.
+# It can span across multiple lines.
+# This is the end."""
+# print(multiLineString)
 
+
+a = int(input("Enter first number: "))
+b = int(input("Enter scond number: ")) 
+sum = a+b
+print("Sum of two numbers is: ",sum)
+
+# Difference between " + " and "," seprator is that in + symbol this is used for concatination of two strings while , symbol is used for seprating multiple arguments.
+# When you use +, it concatenates strings with a variable or another string, you need to ensure that all components are strings.
