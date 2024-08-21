ae4d4f3b5a0e88abc1e126a74707a55f0b4a64cf
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-21 15:48:03 +0500

    "Updated range in for loop from 1-9 to 0-9 and corrected print statement to reflect new range."

--- BreakAndContinue.py
+++ BreakAndContinue.py
@@ -4,9 +4,9 @@ for i in range(1,9):
     print(i)
 print("As the condition is that i == 8 break the loop so the output is 1-7 suspending 8 while range is even from 1-9")
 
-for i in range(1,9):
+for i in range(0,9):
     if i == 1:
         continue
     print(i)
-print("As the condition is that i == 1 continye the loop so the output is 2-8 suspending 1 while range is even from 1-9")
+print("As the condition is that i == 1 continye the loop so the output is 0,2-8 suspending 1 while range is even from 0-9")
 
