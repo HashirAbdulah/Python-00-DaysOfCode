f3a2b436fa32eb081461d8245f6262e6c726c853
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-22 18:08:10 +0500

    Added

--- practiceSet2.py
+++ practiceSet2.py
@@ -1,26 +1,34 @@
-dic = {
-    "choda lagana":"Fuck him out",
-    "zindagi":"Life",
-    "duniya":"World",
-    "pani":"Water",
-    "bharwat":"Making Excuses",
-    "bharwagiri":"Making Unbearable Excuses",
-    "kitaab":"Book",
-}
+# dic = {
+#     "choda lagana":"Fuck him out",
+#     "zindagi":"Life",
+#     "duniya":"World",
+#     "pani":"Water",
+#     "bharwat":"Making Excuses",
+#     "bharwagiri":"Making Unbearable Excuses",
+#     "kitaab":"Book",
+# }
 
-def lookup_word(word):
-    return dic.get(word, "Word not found in the dictionary")
+# def lookup_word(word):
+#     return dic.get(word, "Word not found in the dictionary")
 
 
 
-while True:
-    user_input = input("Enter a word to look up (or type 'exit' to quit): ").strip()
+# while True:
+#     user_input = input("Enter a word to look up (or type 'exit' to quit): ").strip()
     
-    if user_input.lower() == 'exit' and user_input == 'cls':
-        print("Goodbye!")
+#     if user_input.lower() == 'exit' and user_input == 'cls':
+#         print("Goodbye!")
         
-        break
+#         break
     
-    # Lookup and print the result
-    result = lookup_word(user_input)
-    print(f"{user_input}: {result}")
+#     # Lookup and print the result
+#     result = lookup_word(user_input)
+#     print(f"{user_input}: {result}")
+
+set1 = set()
+set1.add(20)
+set1.add(20.0)
+set1.add("20")
+
+print(set1)
+print(len(set1))
