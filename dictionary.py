baef8b6cbf4b46795cc0ad11b24f6eab8ad5f2ac
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-22 14:48:40 +0500

    Added

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# It is a combination of key Value pairs..
+# And ordered collection of Data Items....
+
+generateId = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "employeeId": "476Ah"
+}
+
+print(generateId["name"])
+
+print(generateId.keys())
+print(generateId.values())
+print(generateId.items())
