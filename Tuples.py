105a2e215eae24f3548908152cc3eb4056e8569c
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-27 11:43:36 +0500

    "Added example of duplicating a tuple by multiplying it"

--- Tuples.py
+++ Tuples.py
@@ -5,3 +5,8 @@ for i in range(0,len(tup)):
     
     # Tuples are same as Lists(Arrays) but are immutable and can not be changed
     # Tuples can only be changed ,adding,removing only after converting into list and then again for secure lists change into tuple
+
+#  legal way to turn this tuple: (1,2,3) into this tuple:(1,2,3,1,2,3)?
+tuple1 = (1,2,3)
+tuple1 = tuple1 * 2
+print(tuple1)
