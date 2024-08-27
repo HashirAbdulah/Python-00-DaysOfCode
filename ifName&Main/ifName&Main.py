62990ec28b54b05dc7fff5e9a2a273d54eb0faca
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-27 17:03:04 +0500

    Added

---
+++ ifName&Main/ifName&Main.py
@@ -0,0 +1,7 @@
+# In Python, the statement if __name__ == "__main__": is commonly used to control the execution of code when a Python file is run directly v/s when it is imported as a module in another file. This construct is often referred to as the "main guard" or "script guard."
+
+# ifName&Main_example.py
+
+import module as m
+print("This is ifName&Main_example, running directly.")
+m.say_hello()
---
+++ ifName&Main/module.py
@@ -0,0 +1,7 @@
+def say_hello():
+    print("Hello from module.py file!")
+
+# This code block runs only if this file is executed directly
+if __name__ == "__main__":
+    print("This is module.py, running directly.")
+    say_hello()
