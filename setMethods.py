f5f34d52abe0a92420f3cba70ae18a6f51d4c26e
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-27 11:55:02 +0500

    "Added set add method example and updated set operations"

--- setMethods.py
+++ setMethods.py
@@ -1,6 +1,11 @@
 set1 = {1,2,3,4,6}
 set2 = {1,7,8,5,6}
 
+# set Add Method:
+set1 = {1,2,3,4,6}
+set1.add(7)
+print(set1)
+
 print(set1.union(set2))
 print(set1.intersection(set2))
 set1.update(set2)
@@ -23,3 +28,4 @@ print(cities2.issubset(cities))
 
 # Check more of the sets methods in the repel:
 # https://replit.com/@codewithharry/32-Day32-Set-Methods
+# https://www.w3schools.com/python/python_sets.asp
