60e151cf98314d365aad010fac9ebdf93ce9ba33
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-27 16:17:25 +0500

    Added

---
+++ importFunctions.py
@@ -0,0 +1,17 @@
+# import statement is used to bring in modules and functions from the standard library or other files so that you can use them in your code. A module is simply a file containing Python definitions and statements. You can also import file of your own project as import __filename__ as __yourWord__ ....
+import math as m
+
+# Square root
+a = m.sqrt(9)
+print(a)
+
+#Power/Exponent 
+b = m.pow(2, 3)
+print(b)
+
+# Factorial
+c = m.factorial(5)
+print(c)
+
+
+
