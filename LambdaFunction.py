8964ab3f948bd1546ee347f977cd2de7af0a81b0
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-08-29 16:03:17 +0500

    Added

---
+++ LambdaFunction.py
@@ -0,0 +1,12 @@
+double = lambda x: x**2
+print(double(2))
+
+# Function to calculate the factorial of a number
+
+factorial = lambda number: 1 if number == 0 else number * factorial(number-1)
+print(factorial(5))
+
+
+add = lambda a,b : a+b
+print(add(2,2))
+
