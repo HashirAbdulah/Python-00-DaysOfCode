b4952f8b9ebc3361527ad5d8b126762b84fb147c
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-02 16:43:37 +0500

    Snake Water and Gun Game

---
+++ SnakeWaterGun.py
@@ -0,0 +1,28 @@
+import random
+computer = random.randint(1, 3)
+print("Use 1 for Snake, 2 for Water & 3 for Gun")
+choice = int(input("Enter a Choice between \n 1.Snake \n 2.Water \n 3.Gun \n"))
+if choice == computer:
+    print("It's a tie!")
+    exit()
+elif choice == 1 and computer == 2:
+    print("You Win (Choosed: Snake)! Snake drinks water.")
+    exit()
+elif choice == 2 and computer == 3:
+    print("You Win (Choosed: Water)! Water rusts gun.")
+    exit()
+elif choice == 3 and computer == 1:
+    print("You Win (Choosed: Gun)! Gun shoots snake.")
+    exit()
+elif choice == 1 and computer == 3:
+    print("You Lose! Gun shoots snake.")
+    exit()
+elif choice == 2 and computer == 1:
+    print("You Lose! Snake drinks water.")
+    exit()
+elif choice == 3 and computer == 2:
+    print("You Lose! Water rusts gun.")
+    exit()
+else:
+    print("Invalid input! Please enter 1, 2, or 3.")
+
