2b2c4c42b3eb53723a37ff11054a4cfdd3f0e512
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-04 17:54:06 +0500

    Added

--- OOPs Concepts/Instance&ClassVar.py
+++ OOPs Concepts/Instance&ClassVar.py
@@ -1,12 +1,14 @@
 class Student:
     schoolName = "ABC School" # Class Variable
+    noofStudents = 0
     def __init__(self,name,age,grade):
         self.name = name
         self.age = age
         self.grade = grade
+        Student.noofStudents +=1
 
     def showDetails(self):
-        print(f"Name: {self.name}, age: {self.age}, grade: {self.grade},SchoolName: {self.schoolName}")
+        print(f"Name: {self.name}, age: {self.age}, grade: {self.grade},SchoolName: {self.schoolName},Student count:{self.noofStudents}")
     
 st = Student("Harry",22,"A+")
 st.schoolName = "University of Lahore" # Instance variable
