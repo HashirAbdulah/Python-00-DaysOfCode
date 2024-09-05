761d8cb2119b0d8213569747d34c12a7175d9aea
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-05 15:09:43 +0500

    Added

--- SuperKeyword.py
+++ SuperKeyword.py
@@ -1,6 +1,7 @@
 # The super keyword in Python is used to refer to the parent class (or base class) from within a child class (or subclass). It allows the child class to access methods and attributes of the parent class.
 
-# Syntax: super(ChildClassName, self).methodName()
+# Syntax:   super().__init__()
+ #          super().methodName(with params) 
 
 
 class Person:
@@ -25,3 +26,19 @@ st1 = Student(name, age, grade)
 print("Name:",st1.name)
 print("Age:",st1.age)
 print("Grade:",st1.grade)
+
+
+class Shape:
+    def area(self):
+        return 0
+
+class Rectagle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+    
+rect = Rectagle(5,6)
+print(f"Area: {rect.area()}")
