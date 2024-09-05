ee4b24a50dc3a05dfd3d8df28b7925bae90160f2
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-05 16:43:01 +0500

    Added

---
+++ Magic/DunderMethods.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+
+class Shop:
+    def __init__(self, sales, expenditure):
+        self.sales = sales
+        self.expenditure = expenditure
+
+    def __call__(self):
+        print(F"The total profit is {self.sales-self.expenditure}")
+
+s=Shop(15000,12000)
+s()
+
+
+# For more dunders methods refer to the web
