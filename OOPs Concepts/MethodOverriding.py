5800182a67a35aed91b5ed7135855cfdb7beaa7a
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-06 11:58:41 +0500

    Added

--- OOPs Concepts/MethodOverriding.py
+++ OOPs Concepts/MethodOverriding.py
@@ -1,4 +1,4 @@
-# Method overriding is an ability of any object-oriented programming language that allows a subclass or child class to provide a specific implementation of a method that is already provided by one of its super-classes or parent classes. When a method in a subclass has the same name, the same parameters or signature, and same return type(or sub-type) as a method in its super-class, then the method in the subclass is said to override the method in the super-class.
+# Method overriding is an ability of any object-oriented programming language that allows a subclass or child class to provide a specific implementation of a method that is already provided by one of its super-classes or parent classes. When a method in a subclass has the same name, the same parameters or signature, and same return type(or sub-type) as a method in its super-class, then the method in the subclass is said to override the method in the super-class.(Same method name with same arguments,return type associates with parent and its child class)
 
 class Shape:
     def __init__(self, x, y):
