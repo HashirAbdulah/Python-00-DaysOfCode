e292813fa5c3a292a13415beab34a2972f4e8807
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-18 18:20:26 +0500

    Added

---
+++ MultiThreading.py
@@ -0,0 +1,26 @@
+# MultiThreading is the concept of parallel execution of the tasks
+# E.g: Square function for the different args get the time according to the execution but by using threading these functions can be executed at the same time..
+
+import threading as t
+import time
+def Square(n):
+    time.sleep(n)
+    print(f"Square of {n} is {n*n}")
+
+t4 = t.Thread(target=Square,args=[12])
+t1 = t.Thread(target=Square,args=[5])
+t2 = t.Thread(target=Square,args=[3])
+t3 = t.Thread(target=Square,args=[8])
+
+t4.start()
+t1.start()
+t2.start()
+t3.start()
+
+# Joining the threads ensures that the main thread waits for the other threads to finish before it continues
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+
+print("All threads are completed")
